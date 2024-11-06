0933ad76fa0c54f3db9a5f3c84511daa7e904915
Gregory Vervoort <gregory.vervoort@student.pxl.be>
2024-11-06 23:19:37 +0100

    Discovery service up & running. Added all the services in the discovery

--- backend-java/javaFullStackCase/shop-service/src/main/java/be/pxl/services/ShopServiceApplication.java
+++ backend-java/javaFullStackCase/shop-service/src/main/java/be/pxl/services/ShopServiceApplication.java
@@ -2,12 +2,14 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * ShopServiceApplication
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShopServiceApplication
 {
     public static void main( String[] args )
